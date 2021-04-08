d26b282cc008976155d35482d325ddcab9126b19
ZackStrater <zack.m.strater@gmail.com>
2021-04-08 15:10:34 -0700

    clearing zack notebook

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -14,3 +14,9 @@ movies_df.drop('num', axis=1, inplace=True)
 users_df = pd.read_csv("data/users.dat", sep='::', names=["num", "gender", "age", 'number', 'id'])
 users_df['gender'] = users_df['gender'].map({'F': 1, 'M': 0})
 users_df.drop(['num', 'id'], axis=1, inplace=True)
+
+
+#merging movie_metadata with movies_df 
+movie_metadata = pd.read_csv('data/movies_metadata.csv')
+movies_metadata_df = movies_df.merge(movie_metadata, left_on='title', right_on='title', how='left')
+movies_metadata_df['title']=movies_metadata_df['title'].apply(lambda x: x.split(',')[1].lstrip(' ') + " "+ x.split(',')[0] if ', The' in x else x)
